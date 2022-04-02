3bf611b305a0e0957c2ddee7506feaa046c2caa7
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2022-04-02 18:50:57 +0300

    a class-based approach to writing code. working with forms creating managed components

--- src/components/employess-add-form/employees-add-form.js
+++ src/components/employess-add-form/employees-add-form.js
@@ -1,21 +1,45 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Add a new employee</h3>
-      <form 
-          className="add-form d-flex">
-            <input type="text"
-                className="form-control new-post-label"
-                placeholder="Employee's name?" />
-            <input type="number" className="form-control new-post-label"
-                placeholder="Salary in $?"/>
-             <button className="btn btn-outline-light">Add</button>
 
-       </form>
-    </div>
-  )
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: ''
+    }
+  }
+
+  onValueCahge = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  render () {
+    const {name, salary} = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Add a new employee</h3>
+        <form 
+            className="add-form d-flex">
+              <input type="text"
+                  className="form-control new-post-label"
+                  placeholder="Employee's name?"
+                  name="name"
+                  value={name}
+                  onChange={this.onValueCahge}/>
+              <input type="number" className="form-control new-post-label"
+                  placeholder="Salary in $?"
+                  name="salary"
+                  value={salary}
+                  onChange={this.onValueCahge}/>
+               <button className="btn btn-outline-light">Add</button>
+  
+         </form>
+      </div>
+    )
+  }
 }
 
 export default EmployeesAddForm;
