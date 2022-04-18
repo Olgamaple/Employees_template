1458f66f2847316afa7ff9c02025201449ab713f
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2022-04-18 20:59:42 +0300

    Implemented application functionality: search and filter employees based on input data

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,23 +1,29 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+  const buttonsData = [
+   { name: 'all', label: 'All emploeeys'},
+   { name: 'rise', label: 'To be promoted'},
+   { name: 'moreThen1000', label: 'Salary over $1000'},
+  ];
+
+  const buttons = buttonsData.map(({name, label}) => {
+    const active = props.filter === name;
+    const clazz = active ? 'btn-light' : 'btn-outline-light';
+    return (
+      <button type="button"
+          className={`btn ${clazz}`}
+          key={name}
+          onClick={() => props.onFilterSelect(name)}>
+            {label}
+      </button>
+    )
+  })
+  
+
   return (
     <div className="btn-group">
-      <button 
-          className="btn btn-light"
-          type="button">
-            All employees
-      </button>
-      <button 
-          className="btn btn-outline-light"
-          type="button">
-            To be promoted 
-      </button>
-      <button 
-          className="btn btn-outline-light"
-          type="button">
-            Salary over $1.000
-      </button>
+      {buttons}
       </div>
   );
 }
